e0be43cefaf051d58118e77c69ab8d0fc4f5ae21
Radames Ramirez <rramirez31@ucol.mx>
2022-03-18 10:17:58 -0600

    Hola Mundo

---
+++ Prueba/Program.cs
@@ -0,0 +1,32 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/net6", (string frase) => $".NET 6 es {frase}");
+app.MapGet("sumar/{n1}/{n2}",
+    (int n1, int n2) =>
+    {
+        return n1 + n2;
+    });
+app.MapGet("{n1}/mas/{n2}",
+    (int n1, int n2) =>
+    {
+        return n1 + n2;
+    });
+app.MapGet("{n1}/menos/{n2}",
+    (int n1, int n2) =>
+    {
+        return n1 - n2;
+    });
+app.MapGet("{n1}/entre/{n2}",
+    (float n1, float n2) =>
+    {
+        return n1 / n2;
+    });
+app.MapGet("{n1}/por/{n2}",
+    (int n1, int n2) =>
+    {
+        return n1 * n2;
+    });
+app.Run(); 
+
